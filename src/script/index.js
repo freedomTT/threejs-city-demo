d9c87a01eb3746f1e4edf19e1159e7179bd156fe
luozhongdao <768851314@qq.com>
2021-08-06 17:32:20 +0800

    update

--- src/script/index.js
+++ src/script/index.js
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Reflector } from "three/examples/jsm/objects/Reflector";
 
 export default class ThreeCity {
   constructor() {
@@ -16,6 +17,7 @@ export default class ThreeCity {
     this.createScene(true);
     this.createCamera(true);
     this.createRenderer();
+    // this.setFog();
     this.setControl();
     this.setLight();
     // 加载内容
@@ -27,15 +29,15 @@ export default class ThreeCity {
    * */
   createCamera(addHelper = false) {
     this.camera = new THREE.PerspectiveCamera(
-      10,
+      45,
       window.innerWidth / window.innerHeight,
       1,
-      2000
+      20000
     );
 
     this.camera.position.x = 0;
     this.camera.position.y = 0;
-    this.camera.position.z = 100;
+    this.camera.position.z = 2000;
     this.camera.lookAt(0, 0, 0);
     if (addHelper) {
       const helper = new THREE.CameraHelper(this.camera);
@@ -74,21 +76,34 @@ export default class ThreeCity {
    * */
 
   setLight() {
-    let light = new THREE.AmbientLight("#8b8b8b");
+    let light = new THREE.AmbientLight("#ffffff");
     this.scene.add(light);
 
-    let directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-    directionalLight.position.x = 10;
-    directionalLight.position.y = 10;
-    directionalLight.position.z = 10;
-    this.scene.add(directionalLight);
-    let helper = new THREE.DirectionalLightHelper(directionalLight, 5);
-    this.scene.add(helper);
+    const addDirectionLight = (x, y, z, size, color) => {
+      let directionalLight = new THREE.DirectionalLight(color, 1);
+      directionalLight.position.x = x;
+      directionalLight.position.y = y;
+      directionalLight.position.z = z;
+      this.scene.add(directionalLight);
+      let helper1 = new THREE.DirectionalLightHelper(directionalLight, size);
+      this.scene.add(helper1);
+    };
+    addDirectionLight(200, -200, 150, 500, 0x1352e1); // 右边
+    addDirectionLight(0, 200, 10, 500, 0xffffff); // 后边
+    addDirectionLight(-200, -200, 100, 500, 0xd65ed5); // 左边
+  }
+  /**
+   * @desc 添加场景雾
+   * */
+
+  setFog() {
+    this.scene.fog = new THREE.Fog("#05103D", 0, 2000);
   }
   /**
    * @desc 动画
    * */
   animation(time) {
+    if (this.wayAnimation) this.wayAnimation();
     this.controls.update();
     this.renderer.render(this.scene, this.camera);
   }
@@ -103,23 +118,18 @@ export default class ThreeCity {
    * @desc 创建地面函数
    * */
   makeGround() {
-    const maps = new THREE.TextureLoader().load("/images/bgf.png");
-    maps.wrapS = maps.wrapT = THREE.RepeatWrapping;
-    maps.repeat.set(50, 50); // 纹理 y,x方向重铺
-    maps.needsUpdate = false; // 纹理更新
-    let material = new THREE.MeshBasicMaterial({
-      map: maps,
-      opacity: 1,
-      transparent: true,
-      color: "#6f1fff",
+    const geometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
+
+    let ground = new Reflector(geometry, {
+      clipBias: 0.003,
+      textureWidth: window.innerWidth * window.devicePixelRatio,
+      textureHeight: window.innerHeight * window.devicePixelRatio,
+      color: "#381544",
     });
-    const geometry = new THREE.PlaneGeometry(200, 200, 1, 1);
-    let ground = new THREE.Mesh(geometry, material);
     ground.position.x = 0;
     ground.position.y = 0;
-    ground.position.z = -1;
+    ground.position.z = 4;
     this.scene.add(ground);
-    ground.receiveShadow = true;
   }
   /**
    * @desc 加载环境模型
@@ -127,23 +137,54 @@ export default class ThreeCity {
   loadEvnModel() {
     this.objGroup = new THREE.Group(); // 组
     this.scene.add(this.objGroup);
-    let loader = new FBXLoader();
-    const color = new THREE.Color("#616161");
-    loader.load("/model/cd.fbx", (object) => {
+    let loader = new GLTFLoader();
+    const color = new THREE.Color("#ffffff");
+    loader.load("/model/gx.glb", (glb) => {
+      let object = glb.scene;
       object.rotation.x = Math.PI / 2;
-      object.scale.set(0.0001, 0.0001, 0.0001);
-      object.traverse(function (child) {
+      object.scale.set(1, 1, 1);
+
+      object.traverse((child) => {
         if (child.isMesh) {
-          child.material.transparent = true;
-          child.material.opacity = 0.5;
-          child.material.color = color;
           child.castShadow = true;
+          if (child.name === "Areas") {
+            let tubeMaterial = new THREE.MeshPhongMaterial({
+              transparent: true,
+              opacity: 1,
+              color: "#1A5FEA", // 1A5FEA
+              wireframe: false,
+            });
+            child.material = tubeMaterial;
+          }
+          if (child.name === "Router") {
+            let textureLoader = new THREE.TextureLoader();
+            let texture = textureLoader.load("/images/t2.jpg");
+            // 设置阵列模式为 RepeatWrapping
+            texture.wrapS = THREE.RepeatWrapping;
+            texture.wrapT = THREE.RepeatWrapping;
+            // 设置x方向的偏移(沿着管道路径方向)，y方向默认1
+            // 等价texture.repeat= new THREE.Vector2(20,1)
+            texture.repeat.x = 1;
+            texture.repeat.y = 1;
+            let tubeMaterial = new THREE.MeshBasicMaterial({
+              map: texture,
+              transparent: true,
+              side: THREE.DoubleSide,
+              color: "#C7A6D1",
+              // emissive: 10,
+              // shininess: 50,
+              // ambient: 0xffffff,
+            });
+            child.material = tubeMaterial;
+            this.wayAnimation = () => {
+              texture.offset.x -= 0.02;
+            };
+          }
         }
       });
       this.objGroup.add(object);
-
       this.objGroup.position.x = 0;
-      this.objGroup.position.y = 0; 
+      this.objGroup.position.y = 0;
     });
   }
 }
