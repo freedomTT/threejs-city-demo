450d83996b845c8f5ee4da5e170191e5df7fe659
luozhongdao <768851314@qq.com>
2021-08-09 17:35:48 +0800

    up

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,6 @@
 "use strict";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { MapControls } from "three/examples/jsm/controls/OrbitControls";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Reflector } from "three/examples/jsm/objects/Reflector";
@@ -17,7 +17,7 @@ export default class ThreeCity {
     this.createScene(true);
     this.createCamera(true);
     this.createRenderer();
-    // this.setFog();
+    this.setFog();
     this.setControl();
     this.setLight();
     // 加载内容
@@ -36,8 +36,8 @@ export default class ThreeCity {
     );
 
     this.camera.position.x = 0;
-    this.camera.position.y = 0;
-    this.camera.position.z = 2000;
+    this.camera.position.y = 500;
+    this.camera.position.z = 1000;
     this.camera.lookAt(0, 0, 0);
     if (addHelper) {
       const helper = new THREE.CameraHelper(this.camera);
@@ -59,7 +59,7 @@ export default class ThreeCity {
    * */
   createRenderer() {
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    // this.renderer.setPixelRatio(window.devicePixelRatio * 1);
+    this.renderer.setPixelRatio(window.devicePixelRatio * 1);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setAnimationLoop(this.animation.bind(this));
     document.body.appendChild(this.renderer.domElement);
@@ -76,28 +76,42 @@ export default class ThreeCity {
    * */
 
   setLight() {
-    let light = new THREE.AmbientLight("#ffffff");
-    this.scene.add(light);
+    // 环境光
+    // let light = new THREE.AmbientLight("#ffffff");
+    // this.scene.add(light);
 
-    const addDirectionLight = (x, y, z, size, color) => {
+    // 环境光
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
+    hemiLight.position.set(0, 200, 0);
+    this.scene.add(hemiLight);
+    const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+    this.scene.add(hemiLightHelper);
+
+    const addDirectionLight = (x, y, z, color) => {
       let directionalLight = new THREE.DirectionalLight(color, 1);
       directionalLight.position.x = x;
       directionalLight.position.y = y;
       directionalLight.position.z = z;
       this.scene.add(directionalLight);
-      let helper1 = new THREE.DirectionalLightHelper(directionalLight, size);
+      let helper1 = new THREE.DirectionalLightHelper(directionalLight, 50);
       this.scene.add(helper1);
     };
-    addDirectionLight(200, -200, 150, 500, 0x1352e1); // 右边
-    addDirectionLight(0, 200, 10, 500, 0xffffff); // 后边
-    addDirectionLight(-200, -200, 100, 500, 0xd65ed5); // 左边
+    addDirectionLight(-200, 100, 200, 0x0000ff); // 左边
+
+    // const pointLight = new THREE.PointLight(0xffffff, 1, 5000);
+    // pointLight.position.set(-500, 1000, -500);
+    // this.scene.add(pointLight);
+
+    // const sphereSize = 10;
+    // const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+    // this.scene.add(pointLightHelper);
   }
   /**
    * @desc 添加场景雾
    * */
 
   setFog() {
-    this.scene.fog = new THREE.Fog("#05103D", 0, 2000);
+    this.scene.fog = new THREE.Fog("#000000", 0, 5000);
   }
   /**
    * @desc 动画
@@ -111,7 +125,7 @@ export default class ThreeCity {
    * @desc 创建控制器
    * */
   setControl() {
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls = new MapControls(this.camera, this.renderer.domElement);
   }
 
   /**
@@ -120,15 +134,23 @@ export default class ThreeCity {
   makeGround() {
     const geometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
 
-    let ground = new Reflector(geometry, {
-      clipBias: 0.003,
-      textureWidth: window.innerWidth * window.devicePixelRatio,
-      textureHeight: window.innerHeight * window.devicePixelRatio,
-      color: "#381544",
+    const texture = new THREE.TextureLoader().load("/images/floor.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(1, 1);
+
+    const material = new THREE.MeshStandardMaterial({
+      bumpMap: texture,
+      flatShading: true,
+      color: 0x000000,
+      roughness: 0.5,
+      metalness: 0,
     });
+    const ground = new THREE.Mesh(geometry, material);
     ground.position.x = 0;
     ground.position.y = 0;
-    ground.position.z = 4;
+    ground.position.z = 5;
+    ground.rotation.x = -Math.PI / 2;
     this.scene.add(ground);
   }
   /**
@@ -138,10 +160,8 @@ export default class ThreeCity {
     this.objGroup = new THREE.Group(); // 组
     this.scene.add(this.objGroup);
     let loader = new GLTFLoader();
-    const color = new THREE.Color("#ffffff");
     loader.load("/model/gx.glb", (glb) => {
       let object = glb.scene;
-      object.rotation.x = Math.PI / 2;
       object.scale.set(1, 1, 1);
 
       object.traverse((child) => {
@@ -151,8 +171,10 @@ export default class ThreeCity {
             let tubeMaterial = new THREE.MeshPhongMaterial({
               transparent: true,
               opacity: 1,
-              color: "#1A5FEA", // 1A5FEA
+              color: "#280a52", // 1A5FEA
               wireframe: false,
+              shininess: 75,
+              // emissive: "#280a52",
             });
             child.material = tubeMaterial;
           }
